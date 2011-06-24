47deacf26b69e1c5573ac8848850b05215264926
marano <thiagomarano@gmail.com>
2011-06-23 22:52:54 -0300

    (cv, duda) heavily simplifying interface / getting rid of unnecessary code

--- lib/doom_formatter.rb
+++ lib/doom_formatter.rb
@@ -4,27 +4,16 @@ class DoomFormatter < RSpec::Core::Formatters::BaseFormatter
 
   def initialize output
     super output
-    @count = 0
   end
 
-  def example_passed(example)
-    @count += 1
-  end
-
-  def example_failed(example)
-    @count += 1
-  end
-
-  def example_pending(example)
-    @count += 1
-  end
-
-  def dump_summary duration, p2, p3, p4
-    output.puts generate duration
+  def dump_summary duration, passing, pending, failed
+    maze_params = generate duration, passing + pending + failed
+    output.puts maze_params
+    `theseus #{maze_params} > out.txt`
   end 
 
-  def generate duration=42
-    "-w #{@count} -c #{duration}"
+  def generate duration, examples
+    "-w #{examples} -c #{duration.to_i == 0 ? (duration * 10).to_i : duration.to_i}"
   end
 
 end
--- spec/doom_formatter_spec.rb
+++ spec/doom_formatter_spec.rb
@@ -6,28 +6,18 @@ describe DoomFormatter do
   let (:formatter) { DoomFormatter.new output }
 
   it "should generate a the size of the maze based on the number of test" do
-    10.times do
-      formatter.example_failed(nil)
-      formatter.example_pending(nil)
-      formatter.example_passed(nil)
-    end
-    formatter.generate.should match(/-w 30/)
+    formatter.generate(0, 30).should match(/-w 30/)
   end
 
   it "should generate the cell size based on the duration" do
-    formatter.generate(10).should match(/-c 10/)
+    formatter.generate(10, 0).should match(/-c 10/)
   end
 
   it 'should output the generated theseus string' do
     mock_output = mock
     formatter = DoomFormatter.new mock_output
-    10.times do
-      formatter.example_failed(nil)
-      formatter.example_pending(nil)
-      formatter.example_passed(nil)
-    end
     mock_output.should_receive(:puts).with("-w 30 -c 10")
-    formatter.dump_summary 10, 0, 0, 0
+    formatter.dump_summary 10, 30, 0, 0
   end
  
 end
